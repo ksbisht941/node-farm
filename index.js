ab1b4f68ecb05eef717691b5d53ad490257dd2c7
Kevin <kevin@Kevins-MacBook-Air.local>
2021-08-29 00:24:55 +0530

    package.json script, install express just for demo, demo slugify

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////////////
@@ -38,9 +39,13 @@ const replaceTemplate = require('./modules/replaceTemplate');
 // }
 
 
-
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+// console.log(slugify('Fresh Avocados', {lower: true }));
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
